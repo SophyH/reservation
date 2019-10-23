c08e86d5d81325838672fa879cb68628f6944553
fmohamed99 <mohamed.fatima69@yahoo.fr>
2019-10-23 12:43:44 +0200

    On branch fatima  Changes to be committed: 	new file:   Client.java 	new file:   ClientEI.java 	new file:   ClientMoral.java 	new file:   ClientPhysique.java 	new file:   Login.java 	new file:   Titre.java

---
+++ src/main/java/reservation/model/Titre.java
@@ -0,0 +1,15 @@
+package reservation.model;
+
+public enum Titre {
+	M("monsieur"), MME("madame"), MLLE("madememoiselle"), SA("Société Anonyme"), SARL("Société à responsabilite limitée"), ;
+	
+	private String label;
+	
+	private Titre(String label) {
+		this.label = label;
+	}
+	
+	public String getLabel() {
+		return label;
+	}
+}
